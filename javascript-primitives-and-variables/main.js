26f6fa829702c94011890e3e07da1e6404b7593e
Alex Raymond <alexjacobraymond@gmail.com>
2022-12-12 17:34:18 +0000

    javascript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,16 @@
+var fullName = 'Alex Raymond';
+var isCool = false;
+var totalPets = 2;
+var unknown = null;
+var never;
+
+console.log('my name: ', fullName);
+console.log('am i cool: ', isCool);
+console.log('how many pets do i live with: ', totalPets);
+console.log('unknown', unknown);
+console.log('name typeof:', typeof fullName);
+console.log('cool typeof:', typeof isCool);
+console.log('pets typeof:', totalPets);
+console.log('unknown typeof', typeof unknown);
+console.log(never);
+console.log('never typeof', typeof never);
