6f65c29e5367619d6e5bd085cf8a85f66576b653
Alex Raymond <alexjacobraymond@gmail.com>
2022-12-15 19:33:46 +0000

    scratch-folder

---
+++ scratch-folder/main.js
@@ -0,0 +1,12 @@
+// var count = 1;
+// count++ + ++count;
+// // 1 + 3 = 4
+
+// var count = 1;
+// count++ + count++ + ++count + ++count + count++;
+// // result of the expression = 17
+// // counter = 6
+// // this is bullshit code, never do this ever
+// // learning tool for ++
+
+// // ++ after count? add 1 after.  ++ before? add it before.
