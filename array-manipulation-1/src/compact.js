32fb87354422f1dd45919f5ca1890e0e376a5b49
Alex Raymond <alexjacobraymond@gmail.com>
2022-12-18 21:09:45 +0000

    array-manipulation-1

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -6,31 +6,54 @@
 // we give back the array with the nice things
 
 // pseudo code with codey words
+// define new blank array
+// for loop going through length of array
+// original idea WAS to filter out each variable, but
+// ... I re read the syntax mdn section of if statements
+// ... and if the condition is truthy then it executes
+// so, just having the array at i being the conditional will output truthys and bypass all the bad
+// push to new array
+// gg
 
 function compact(array) {
   var newArr = [];
-
   for (var i = 0; i < array.length; i++) {
-    if (array[i] === true) {
+    if (array[i]) {
       newArr.push(array[i]);
-    } if (i === array.length - 1) {
-      return newArr;
     }
-  }
-  // newArr = [];
-  // for (i = 0; i < array.length; i++) {
-  //   if (array[i] === {}) {
-  //     newArr.push(array[i]);
-  //   }
-  // }
-  // newArr = [];
-  // for (i = 0; i < array.length; i++) {
-  //   if (array[i] === typeof Number) {
-  //     newArr.push(array[i]);
-  //   }
-  // }
 
+  }
+  return newArr;
 }
+
+// var newArr = [];
+
+// for (var i = 0; i < array.length; i++) {
+//   if (array[i] === false || array[i] === undefined || array[i] == null || array[i] == isNaN(true)) {
+//   } else {
+//     newArr.push(array[i]);
+//   }
+// }
+// return newArr;
+
+//   if (array[i] === true) {
+//     newArr.push(array[i]);
+//   } if (i === array.length - 1) {
+//     return newArr;
+//   }
+// }
+// newArr = [];
+// for (i = 0; i < array.length; i++) {
+//   if (array[i] === {}) {
+//     newArr.push(array[i]);
+//   }
+// }
+// newArr = [];
+// for (i = 0; i < array.length; i++) {
+//   if (array[i] === typeof Number) {
+//     newArr.push(array[i]);
+//   }
+// }
 
 // if (array[i] === false || array[i] === null || array[i] === undefined) { } else {
 //   newArr.unshift(array[i]);
