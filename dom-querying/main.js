1f997043d85211f15c43d3828c8d8945feada981
Alex Raymond <alexjacobraymond@gmail.com>
2022-12-19 17:32:35 +0000

    dom-querying

---
+++ dom-querying/main.js
@@ -0,0 +1,13 @@
+console.log('hi');
+
+console.log('h1 log', document.querySelector('h1'));
+console.dir('h1 dir', document.querySelector('h1'));
+
+console.log('#explanation log', document.querySelector('#explanation'));
+console.dir('#explanation dir', document.querySelector('#explanation'));
+
+console.log('.hint selector', document.querySelector('.hint'));
+console.dir('.hint dir', document.querySelector('.hint'));
+
+console.log('p nodelist', document.querySelectorAll('p'));
+console.log('example-link log', document.querySelectorAll('.example-link'));
