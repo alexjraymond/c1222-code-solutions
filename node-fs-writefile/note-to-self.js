95ba8bed3b1478a92da9182f91cd3c87b7eaeea7
Alex Raymond <alexjacobraymond@gmail.com>
2023-01-25 22:47:13 +0000

    made the changes suggested

--- node-fs-writefile/note-to-self.js
+++ node-fs-writefile/note-to-self.js
@@ -1,14 +1,6 @@
 const fs = require('fs');
 
-const note = process.argv;
-let data = '';
-for (var i = 2; i < note.length; i++) {
-  const noteArray = note[i];
-  data = data + ' ' + noteArray + ' ';
-  if (i === note.length - 1) {
-    data = data + '\n';
-  }
-}
+const data = process.argv[2];
 
 fs.writeFile('note.txt', data, err => {
   if (err) throw err;
