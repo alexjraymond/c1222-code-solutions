1869edad7885c84044170ac9c77ffc20764e6bb1
Alex Raymond <alexjacobraymond@gmail.com>
2023-01-27 18:28:00 +0000

    hello express

--- express-hello-world/index.js
+++ express-hello-world/index.js
@@ -5,6 +5,17 @@ const port = 3000;
 
 app.use(function (req, res, next) {
   console.log('Time:', Date.now());
+
   next();
+
   // res.send
+});
+
+app.use(function (req, res) {
+  res.send('hello world');
+  console.log(req.method);
+});
+
+app.listen(3000, () => {
+  console.log('express server listening on port: 3000');
 });
