4965e47731d39386d0091c975e41488e7aa57e7c
Alex Raymond <alexjacobraymond@gmail.com>
2023-02-08 00:23:14 +0000

    removed the cringe var

--- react-validated-input-component/src/index.jsx
+++ react-validated-input-component/src/index.jsx
@@ -5,18 +5,29 @@ const root = reactDOM.createRoot(container);
 
 function PasswordField() {
   const [password, setPassword] = useState('');
-  if (password.length >= 8) {
-    var isGoodEnough = !!password;
-  }
+  const isGoodEnough = password.length >= 8;
+
+  const passwordHelper = () => {
+    if (password.length === 0) {
+      return <span className='red-text'>a password is required</span>;
+    } if (password.length <= 7) {
+      return <span className='red-text'>password too short</span>;
+    } if (password.length > 7) {
+      return '';
+    }
+  };
 
   const handlePassword = (event) => {
     setPassword(event.target.value);
   };
+
   const iconClass = isGoodEnough ? 'fa-check' : 'fa-xmark';
+
   return (
     <div>
       <input type="password" onChange={handlePassword} value={password}></input>
       <i className={`fa-sharp fa-solid ${iconClass}`}></i>
+      <p >{passwordHelper()}</p>
     </div>
   );
 }
